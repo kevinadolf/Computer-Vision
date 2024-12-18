8d3e57e0e28775f5b1893989a567f75be1ffc488
Kevin Carvalho <kevinadolfocarvalhokob@gmail.com>
2024-12-18 08:44:55 -0300

    Simple Motion Filtering code
    Motion Filtering is just a way to extract and visualize the moving parts of a video.
    Made using OpenCV Python Library.

---
+++ motionFilter.py
@@ -0,0 +1,21 @@
+import cv2 as cv
+
+video = cv.VideoCapture(0)
+subtractor = cv.createBackgroundSubtractorMOG2(20, 50)
+
+while True:
+    
+    ret, frame = video.read()
+
+    if ret:
+        mask = subtractor.apply(frame)
+        cv.imshow('Mask', mask)
+
+        if cv.waitKey(5) == ord('X'):
+            break
+    
+    else:
+        video = cv.VideoCapture(0)
+
+cv.destroyAllWindows()
+video.release()
